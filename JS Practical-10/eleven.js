681f7174d297d43cd0a5f1f819e5569b5ccad5c0
Hameed-usman <usman.hameed1145@gmail.com>
2024-09-19 17:10:29 +0500

    QUQOOSHWH

---
+++ JS Practical-10/eleven.js
@@ -0,0 +1,21 @@
+
+// alert("Hellloe")
+
+// CLICK EVENT 
+
+// let likeCount = 0;
+// function like() {
+//   likeCount++;
+//   document.getElementById("likeCount").textContent = likeCount;
+// }
+
+
+// <!-- DB CLICK EVENT  -->
+
+// function zoomImage() {
+//     const image = document.getElementById("galleryImage");
+//     image.style.transform = image.style.transform === "scale(2)" ? "scale(1)" : "scale(2)";
+//   }
+
+
+  
