843580c6fd691aae2ae5dff6a7e703ad09a3562d
Hameed-usman <usman.hameed1145@gmail.com>
2024-09-19 16:32:23 +0500

    nine

---
+++ nine.js
@@ -0,0 +1,70 @@
+// console.log("Funciton Topic BeginSSSS!");
+
+// ASSIGNMENT 01 
+
+// function addNumbers(num1, num2) {
+//     return num1 + num2;
+// }
+
+// console.log(addNumbers(2, 3)); 
+// console.log(addNumbers(5, 1));
+// console.log(addNumbers(10, 15)); 
+
+// ASISGNEMNT 02 
+
+// function toUpper(upr) {
+//     return upr.toUpperCase();
+// }
+
+// console.log(toUpper("Saylani")); 
+// console.log(toUpper("Usman"));
+// console.log(toUpper("tathteer")); 
+// console.log(toUpper("peshawar"));
+
+// ASSIGNMENT 03 
+
+// function averageArray(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//       sum += arr[i];
+//     }
+//     return sum / arr.length;
+//   }
+//     alert(averageArray([1, 2, 3])); 
+//   alert(averageArray([5, 5, 5])); 
+  
+
+// ASSIGNMENT 04 
+
+// function removeVowels(vwl) {
+//     return vwl.replace(/[aeiouAEIOU]/g, '');  // "We have Used (/g) For Global Scope "
+//   }
+  
+//   console.log(removeVowels("hello world"));
+//   console.log(removeVowels("javascript"));
+
+// ASSIGNMENT 05 
+
+// function factorial(n) {
+//     if (n === 0 || n === 1) {
+//       return 1;
+//     } else {
+//       return n * factorial(n - 1);
+//     }
+//   }
+//   console.log(factorial(5)); 
+//   console.log(factorial(3));
+
+
+// ASSIGNMENT 06 
+
+// function reverseString(INPT) {
+//     if (INPT === '') {
+//       return '';
+//     } else {
+//       return reverseString(INPT.slice(1)) + INPT[0];
+//     }
+//   }
+  
+//   console.log(reverseString("hameed")); 
+//   console.log(reverseString("usman"));
